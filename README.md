47cb181f204786518ac1540e319d2da0539c8654
RekiDunois <RekiDunois+github@gmail.com>
2021-06-30 18:19:53 +0800

    still try to figure out what is Pick

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ts-type-gymnastics
 
-the TypeScript playground, store the [type-challenges](https://github.com/type-challenges) answer write by my own 
+the TypeScript playground, store the [type-challenges](https://github.com/type-challenges) answer write by my own
--- src/Pick.ts
+++ src/Pick.ts
@@ -1,4 +1,4 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends T> = { [P in K]: T[P] }
 
 
 /* _____________ Test Cases _____________ */
@@ -26,3 +26,38 @@ interface Expected2 {
     completed: boolean
 }
 
+// in operator https://www.typescriptlang.org/docs/handbook/2/narrowing.html#the-in-operator-narrowing
+// extend keyword https://www.typescriptlang.org/docs/handbook/2/classes.html#extends-clauses
+// keyof operator https://www.typescriptlang.org/docs/handbook/2/keyof-types.html#the-keyof-type-operator
+
+function useInOperator(params: Expected1 | Expected2) {
+    if ('completed' in params) {
+        return 'It is Expected2';
+    }
+
+    return 'It is Expected1';
+}
+
+function useExtendKeyword(params: Expected1 | Expected2) {
+    interface exFromTodo extends Todo {
+
+    }
+
+    let foo: exFromTodo;
+    let bar: exFromTodo;
+    console.log(foo.completed);
+    // keyof operator
+    type P = keyof exFromTodo;
+    // keyValuePair type
+    // type keyValuePair = { [key: string]: number };
+    interface NumberKey {
+        12: string
+    }
+    type keys = 'string';
+    type keyValuePair = {
+        [K in keys]: exFromTodo
+    }
+    let kv: keyValuePair = { 'string': foo, 'strings': bar }
+
+
+}
